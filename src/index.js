7792f83b63a9986e5ed37e50ad46af66e94289d6
Khanjan27Parikh <developerkhanju09@gmail.com>
2022-08-16 21:42:01 +0530

    Dynamic_Greeting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
+// challenge 3
+// Greeting Website
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let currDate = new Date(2022, 8, 16, 20);
+currDate = currDate.getHours();
+let greeting = '';
+const cssStyle = { };
+
+if(currDate >= 1 && currDate < 12){
+  greeting = 'Good Morning';
+  cssStyle.color = 'green';
+}
+else if(currDate >= 12 && currDate < 16){
+  greeting = 'Good Afternoon';
+  cssStyle.color = 'red';
+}
+else if(currDate >= 16 && currDate < 20){
+  greeting = 'Good Evening';
+  cssStyle.color = 'orange';
+}
+else{
+  greeting = 'Good Night';
+  cssStyle.color = 'blue';
+}
+
+ReactDOM.render(
+  <>
+    <div>
+      <h1>Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
+    </div>
+  </>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
